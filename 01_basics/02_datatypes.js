e5097a2211b69c51c516148c15d71334defd97e7
Piyush <86055402+Piyushshrivas@users.noreply.github.com>
2024-02-27 10:50:04 +0000

    watch_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strct"; //treat all js code as newer js/version 
+
+// alert(2+7) // not work here, bcz we are using nodejs not browser
+
+console.log(3+
+    3); //code readibility should be high
+console.log("hiii");
+
+let name = "watch"
+let age = 22
+let isLoggedIn = true
+
+//datatypes
+//number => 2 to power 53
+//bigint
+//string=> '' / ""
+//boolean => true/false
+//null => a standalone value in js / a representation of empty value & can be assigned as null 
+//undefined => we declared variable but not initialize any value 
+//symbol => used mostly in react & identify unique component among all components
+//object
+
+console.log(typeof "watch"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
